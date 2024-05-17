b826ad782a852475ac1d5fa3f70de3dbb4bfe971
filin121666 <mur.my.75@mail.ru>
2024-05-17 22:02:22 +0300

    main

--- python_code/src/task.py
+++ python_code/src/task.py
@@ -1,5 +1,21 @@
-ans = [1]
-for _ in range(102):
-    n = sum(map(int, str(ans[-1])))
-    ans.append(ans[-1] + 2 - (n % 2))
-print(len(list(filter(lambda i: i % 2 == 0, ans))))
+def sum_digits(number):
+    result = 0
+    while number > 0:
+        result += number % 10
+        number //= 10
+    return result
+
+sequence = [1]+[0]*102
+result = 0
+
+for i in range(0, 103):
+    if sum_digits(sequence[i-1]) % 2 == 0:
+        sequence[i] = sequence[i-1] + 2
+    else:
+        sequence[i] = sequence[i-1] + 1
+
+for i in range(0, 103):
+    if sequence[i] % 2 == 0:
+        result += 1
+
+print(result)
