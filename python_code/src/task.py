4aef6199e4fb6695bb35f34056aa9a0736451db2
filin121666 <mur.my.75@mail.ru>
2024-05-30 14:09:47 +0300

    main

--- python_code/src/task.py
+++ python_code/src/task.py
@@ -8,7 +8,7 @@ def sum_digits(number):
 sequence = [1]+[0]*102
 result = 0
 
-for i in range(0, 103):
+for i in range(1, 103):
     if sum_digits(sequence[i-1]) % 2 == 0:
         sequence[i] = sequence[i-1] + 2
     else:
